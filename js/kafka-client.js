924c6682ddcaf719b04913596059b1486472c588
André Biseth <andre@biseth.net>
2024-12-01 22:40:28 +0000

    feat: add self-signed certificate option for SASL SSL configuration

--- js/kafka-client.js
+++ js/kafka-client.js
@@ -40,12 +40,15 @@ module.exports = function(RED) {
         }
 
         else if(config.auth == 'sasl'){
-            options.ssl = config.saslssl;
+            options.ssl = config.saslssl ? {} : false;
 
             options.sasl = new Object();
             options.sasl.mechanism = config.saslmechanism || 'plain';
             options.sasl.username = node.credentials.saslusername;
             options.sasl.password = node.credentials.saslpassword;
+            if(config.saslssl){
+                options.ssl.rejectUnauthorized = !config.saslselfsign;
+            }
         }
     
         node.options = options;
